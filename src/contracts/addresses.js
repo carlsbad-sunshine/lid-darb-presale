9a52003babd8dc3cbd1b19747d5453ccead3332c
Carlsbad <carlsbad_sunshine@protonmail.com>
2020-08-19 18:11:43 -0700

    Update addresses and abis

--- src/contracts/addresses.js
+++ src/contracts/addresses.js
@@ -1,10 +1,12 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  presale: '0xC1c236950149Ca9955AE1a3505cbe9F4BBC26Bdc',
-  redeemer: '0x87793DE4251Aa9d43CF5AC6b1671AA9f7CB631d1',
-  timer: '0x39e2Cb112ee00B7ebBa78081b25766916317C491',
-  token: '0x2129ff6000b95a973236020bcd2b2006b0d8e019'
+  presale: '0x1B6c9791AD7DF49258753061b8730761B0e6b4Fa',
+  daofund: '0x1B6c9791AD7DF49258753061b8730761B0e6b4Fa',
+  teamfund: '0xCf9d7c25baf4ea6D75B47f2E8cc5E0c5fa1c2dAC',
+  redeemer: '0x86A655543eeD582f81A7acE3687735f30e2C97a1',
+  timer: '0x571bD0cF4f3cde88080a39AC8D963755f25C5002',
+  token: '0x1332d391f484834bc3285ac50dbb55e3eac47ac5'
 };
 
 export default addresses;
